ad532ff698ec0db13461aeabb54880bc929b11d3
sadpepesadsing <90351901+sadpepesadsing@users.noreply.github.com>
2024-12-12 00:47:21 +0300

    fixing bugs

--- bot_func.py
+++ bot_func.py
@@ -1,8 +1,19 @@
 import telebot
-from telebot import types
+
+from bot_btns import *
+user_currency = {}
 
 token = "7710098454:AAGn-yU94cI96EoFCtieXGJLilQ-F5spWns"
 bot = telebot.TeleBot(token)
 
-def amount_msg(message, currency):
-    bot.delete_message(message.chat.id, message.message_id - 1)
+def amount_msg(message):
+    message.text = message.text.replace(',', '.')
+    try:
+        bot.send_message(message.chat.id, f'{message.text} {user_currency[message.chat.id][0]} = '
+                                          f'{round(float(message.text) * float(user_currency[message.chat.id][1]), 3)} RUB',
+                         reply_markup=menu_btns)
+    except TypeError:
+        bot.send_message(message.chat.id, 'Ваше сообщение не является числом', reply_markup=menu_btns)
+
+    bot.delete_message(message.chat.id, message.message_id)
+    bot.delete_message(message.chat.id, message.message_id-1)
--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-import telebot
-
-from bot_func import amount_msg
+from bot_func import *
 from Currency import find_currency
-from bot_btns import *
-
-token = "7710098454:AAGn-yU94cI96EoFCtieXGJLilQ-F5spWns"
-bot = telebot.TeleBot(token)
 
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
+    bot.send_message(message.chat.id, 'Добрый день, какая валюта вас интересует?', reply_markup=hello_btns)
+@bot.message_handler(content_types=['text'])
+def any_message(message):
     bot.delete_message(message.chat.id, message.message_id)
+    bot.delete_message(message.chat.id, message.message_id-1)
     bot.send_message(message.chat.id, 'Добрый день, какая валюта вас интересует?', reply_markup=hello_btns)
 
 @bot.callback_query_handler(func=lambda call: True)
@@ -18,23 +16,26 @@ def answer(call):
     if call.data == 'euro':
         bot.delete_message(call.message.chat.id, call.message.message_id)
         euro = find_currency('EUR')
+        user_currency[call.message.chat.id] = ('EUR', euro.replace(',', '.'))
         bot.send_message(call.message.chat.id, f"1€ = {euro}₽", reply_markup=menu_btns)
 
     if call.data == 'dollar':
         bot.delete_message(call.message.chat.id, call.message.message_id)
         usd = find_currency('USD')
+        user_currency[call.message.chat.id] = ('USD', usd.replace(',', '.'))
         bot.send_message(call.message.chat.id, f"1$ = {usd}₽", reply_markup=menu_btns)
     if call.data == 'som':
         bot.delete_message(call.message.chat.id, call.message.message_id)
         uzs = find_currency('UZS')
-        bot.send_message(call.message.chat.id, f"1 UZS = {uzs/10000}₽", reply_markup=menu_btns)
+        user_currency[call.message.chat.id] = ('UZS', str(float(uzs.replace(',', '.'))/10000))
+        bot.send_message(call.message.chat.id, f"10000 UZS = {uzs}₽", reply_markup=menu_btns)
     if call.data == 'back':
         bot.delete_message(call.message.chat.id, call.message.message_id)
         bot.send_message(call.message.chat.id, 'Добрый день, какая валюта вас интересует?', reply_markup=hello_btns)
     if call.data == 'calc':
         bot.delete_message(call.message.chat.id, call.message.message_id)
-        amount = bot.send_message(call.message.chat.id, 'Введите сумму в УЕ, чтобы узнать конвертацию в рублях (только целые числа)')
-        bot.register_next_step_handler(amount, amount_msg())
+        amount = bot.send_message(call.message.chat.id, 'Введите сумму в УЕ, чтобы узнать конвертацию в рублях (только числа)')
+        bot.register_next_step_handler(amount, amount_msg)
 
 
 bot.infinity_polling()
