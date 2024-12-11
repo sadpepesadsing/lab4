2fc8e3856d82f5bb96c9764a968289e4a992feff
sadpepesadsing <90351901+sadpepesadsing@users.noreply.github.com>
2024-12-12 00:55:18 +0300

    fixing bugs

--- bot_func.py
+++ bot_func.py
@@ -12,7 +12,7 @@ def amount_msg(message):
         bot.send_message(message.chat.id, f'{message.text} {user_currency[message.chat.id][0]} = '
                                           f'{round(float(message.text) * float(user_currency[message.chat.id][1]), 3)} RUB',
                          reply_markup=menu_btns)
-    except TypeError:
+    except ValueError:
         bot.send_message(message.chat.id, 'Ваше сообщение не является числом', reply_markup=menu_btns)
 
     bot.delete_message(message.chat.id, message.message_id)
